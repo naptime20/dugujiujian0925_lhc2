6158b55362cceec8892679e865987bef9e052e09
“sunchan” <sunnychaan@qq.com>
2020-07-23 20:06:27 +0800

    nap time 修改

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -7,5 +7,7 @@ package com.atguigu.jianfa.dugujiujian;
 public class Dugu  {
     public static void main(String[] args){
         System.out.println("jianfa dugujiujian!");
+        System.out.println("jianfa dugujiujian!");
+        System.out.println("jianfa dugujiujian!");
     }
 }
