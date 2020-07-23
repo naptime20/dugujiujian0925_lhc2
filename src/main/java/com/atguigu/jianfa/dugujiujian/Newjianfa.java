37ae4fed8fe3798d4714a2b9986ecd4102dae787
“sunchan” <sunnychaan@qq.com>
2020-07-23 20:57:59 +0800

    新剑法

---
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa.dugujiujian;
+
+/**
+ * @author sunny
+ * @create 8:40 PM 2020/7/23
+ **/
+public class Newjianfa {
+    public static void main(String[] args){
+        System.out.println("新剑法！ ");
+    }
+}
+
